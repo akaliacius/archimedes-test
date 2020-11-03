082e79c230d70b2fff11224616c0a3259a7fb4b7
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2020-11-03 18:58:52 +0000

    adding enums for report type and name

--- src/main/java/archimedes/test/reporting/BasicReportProducer.java
+++ src/main/java/archimedes/test/reporting/BasicReportProducer.java
@@ -1,10 +1,30 @@
 package archimedes.test.reporting;
 
+import archimedes.test.reporting.generators.ReportGenerator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BasicReportProducer implements ReportProducer {
+
+    // we should be able to support multiple types of reports
+    enum ReportType{
+        CSV_REPORT("csvReport");
+        public final String type;
+        ReportType(String type){
+            this.type = type;
+        }
+    }
+
+    // also generate different reports in future
+    enum ReportName {
+        CALL_OPERATOR_REPORT("callOperatorReportGen");
+        public final String reportName;
+        ReportName(String reportName){
+            this.reportName = reportName;
+        }
+    }
+
     private final ApplicationContext context;
 
     public BasicReportProducer(ApplicationContext context) {
@@ -14,7 +34,8 @@ public class BasicReportProducer implements ReportProducer {
     @Override
     public void makeReport(){
         var request = ReportRequest.builder().build();
-        var report = context.getBean(CsvReport.class);
-        report.generate(request, context.getBean(CallOperatorReportGen.class));
+        var report = context.getBean(ReportType.CSV_REPORT.type, Report.class);
+        report.generate(request, context.getBean(ReportName.CALL_OPERATOR_REPORT.reportName,
+                ReportGenerator.class));
     }
 }
