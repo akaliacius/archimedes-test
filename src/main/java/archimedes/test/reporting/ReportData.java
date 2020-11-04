d98c65c9740584e9f40797a0dde18c8c48023591
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2020-11-04 01:09:07 +0000

    report data to hold headers and values

--- src/main/java/archimedes/test/reporting/ReportData.java
+++ src/main/java/archimedes/test/reporting/ReportData.java
@@ -1,16 +1,41 @@
 package archimedes.test.reporting;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class ReportData {
     private String name;
+    private Map<String, List<String>> reportData;
 
     private ReportData() {
+        reportData = new LinkedHashMap<>();
+    }
+
+    public void addReportEntry(String key, String value){
+        var list = reportData.get(key);
+        if(Objects.isNull(list)) {
+            list = new ArrayList<>(Arrays.asList(value));
+            reportData.put(key, list);
+        } else {
+            list.add(value);
+        }
+    }
+
+    public List<String> getHeaders(){
+        return reportData.entrySet().stream()
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public Map<String, List<String>> getReportData() {
+        return reportData;
     }
 
     public static Builder builder(){
         return new Builder();
     }
 
-    static class Builder {
+    public static class Builder {
         private final ReportData data;
 
         private Builder() {
